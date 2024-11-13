c4830dd0ca4a043114044ba18b2469e527cae749
jpbertti <joaobertty@gmail.com>
2024-11-13 11:53:17 -0300

    Adicionar arquivos de projeto.

---
+++ operacoes_aritimeticas_ex1/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace operacoes_aritimeticas_ex1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblA = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.lblB = new System.Windows.Forms.Label();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.lblC = new System.Windows.Forms.Label();
+            this.txtD = new System.Windows.Forms.TextBox();
+            this.lblD = new System.Windows.Forms.Label();
+            this.txtE = new System.Windows.Forms.TextBox();
+            this.lblE = new System.Windows.Forms.Label();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(124, 33);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(41, 13);
+            this.lblA.TabIndex = 0;
+            this.lblA.Text = "Valor A";
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(96, 49);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 20);
+            this.txtA.TabIndex = 1;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(96, 98);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 20);
+            this.txtB.TabIndex = 3;
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(125, 82);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(41, 13);
+            this.lblB.TabIndex = 2;
+            this.lblB.Text = "Valor B";
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(96, 146);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(100, 20);
+            this.txtC.TabIndex = 5;
+            // 
+            // lblC
+            // 
+            this.lblC.AutoSize = true;
+            this.lblC.Location = new System.Drawing.Point(124, 130);
+            this.lblC.Name = "lblC";
+            this.lblC.Size = new System.Drawing.Size(41, 13);
+            this.lblC.TabIndex = 4;
+            this.lblC.Text = "Valor C";
+            // 
+            // txtD
+            // 
+            this.txtD.Location = new System.Drawing.Point(96, 197);
+            this.txtD.Name = "txtD";
+            this.txtD.Size = new System.Drawing.Size(100, 20);
+            this.txtD.TabIndex = 7;
+            // 
+            // lblD
+            // 
+            this.lblD.AutoSize = true;
+            this.lblD.Location = new System.Drawing.Point(123, 181);
+            this.lblD.Name = "lblD";
+            this.lblD.Size = new System.Drawing.Size(42, 13);
+            this.lblD.TabIndex = 6;
+            this.lblD.Text = "Valor D";
+            // 
+            // txtE
+            // 
+            this.txtE.Location = new System.Drawing.Point(96, 243);
+            this.txtE.Name = "txtE";
+            this.txtE.Size = new System.Drawing.Size(100, 20);
+            this.txtE.TabIndex = 9;
+            // 
+            // lblE
+            // 
+            this.lblE.AutoSize = true;
+            this.lblE.Location = new System.Drawing.Point(124, 227);
+            this.lblE.Name = "lblE";
+            this.lblE.Size = new System.Drawing.Size(41, 13);
+            this.lblE.TabIndex = 8;
+            this.lblE.Text = "Valor E";
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(33, 342);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 10;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(184, 342);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 11;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 389);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.txtE);
+            this.Controls.Add(this.lblE);
+            this.Controls.Add(this.txtD);
+            this.Controls.Add(this.lblD);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.lblC);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.lblA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.Label lblC;
+        private System.Windows.Forms.TextBox txtD;
+        private System.Windows.Forms.Label lblD;
+        private System.Windows.Forms.TextBox txtE;
+        private System.Windows.Forms.Label lblE;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ operacoes_aritimeticas_ex1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace operacoes_aritimeticas_ex1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja realmente sair?", "Sair", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                Close();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double valorA, valorB, valorC, valorD, valorE, somaA, somaB, somaC;
+
+            valorA = double.Parse(txtA.Text);
+            valorB = double.Parse(txtB.Text);
+            valorC = double.Parse(txtC.Text);
+            valorD = double.Parse(txtD.Text);
+            valorE = double.Parse(txtE.Text);
+
+            somaA = (valorA + valorB) * valorC;
+            somaB = (somaA) / 5;
+            somaC = valorA - valorB * (valorC + valorD * valorD) / valorE;
+
+            MessageBox.Show("O Resultado de A + B * C é: " + somaA);
+            MessageBox.Show("O Resultado do calculo anterior / pela quantidade informada (5) é: " + somaB);
+            MessageBox.Show("A – B(C+D²) /E é: " + somaC);
+
+
+        }
+    }
+}
